c21c1b52c5f626f9c7c2edfb7bb3c9d58a73121d
davi <davijluna@gmail.com>
2023-07-17 22:33:01 -0300

    fix: código alterado momentâniamente para teste de calendarios

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// import 'test' from './tratamentos';
+
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl1 = document.getElementById('source-calendar');
   var calendarEl2 = document.getElementById('destination-calendar');
   var calendarEl3 = document.getElementById('tree-calendar');
 //  criamos uma variável com a função para setar.
   var uniqueEvents = new Set();
+
 
   function getEventContent(eventInfo) {
     var event = eventInfo.event;
@@ -25,58 +28,105 @@ document.addEventListener('DOMContentLoaded', function() {
     var mes = ("0" + (dataAtual.getMonth() + 1)).slice(-2);
     var dia = ("0" + dataAtual.getDate()).slice(-2);
     var dataFormatada = ano + "-" + mes + "-" + dia;
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,dayGridDay,listMonth'
-      },
-      initialView: 'dayGridDay',
-      locale: 'pt-br',
-      initialDate: dataFormatada,
-      editable: false,
-      eventContent: getEventContent,
-      eventSources: [
-        {
-          url: 'http://difiores-001-site3.etempurl.com/api/Agenda',
-          success: function(data) {
-            var events = data.filter(function(event) {
-              return event.place === place;
-            });
-              events.forEach(event => {
-              event.beginEvent = event.start;
-              event.endEvent = event.end;
-              delete event.start;
-              delete event.end;
-            });
-             // buscamos o valor que vem do JSON em event 
-             // guardamos os ids em uma váriavel e usamos o método has para verificar existência do id no json
-             // caso não exista ele será adicionado 
-             // e em seguida chamamos a o método addEventSource para montar o calendário na tela. 
-              events.forEach(function(event) {
-              var eventId = event.id;
-              if (!uniqueEvents.has(eventId)) {
-                uniqueEvents.add(eventId);
-                calendar.addEventSource(event);
-          }
-          });
-          }
-        }
-      ],
-      eventLeave: function(info) {
-        console.log('event left!', info.event);
-      },
-    });
 
-    return calendar;
-  }
+      
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//       headerToolbar: {
+//         left: 'prev,next today',
+//         center: 'title',
+//         right: 'dayGridMonth,timeGridWeek,dayGridDay,listMonth'
+//       },
+//       initialView: 'dayGridDay',
+//       locale: 'pt-br',
+//       initialDate: dataFormatada,
+//       editable: false,
+//       eventContent: getEventContent,
+//       eventSources: [
+//         {
+//           // url: 'http://difiores-001-site3.etempurl.com/api/Agenda',
+          
+//           success: function(data) {
+//             var events = data.filter(function(event) {
+//               return event.place === place;
+//             });
+//               events.forEach(event => {
+//               event.beginEvent = event.start;
+//               event.endEvent = event.end;
+//               delete event.start;
+//               delete event.end;
+//             });
+//              // buscamos o valor que vem do JSON em event 
+//              // guardamos os ids em uma váriavel e usamos o método has para verificar existência do id no json
+//              // caso não exista ele será adicionado 
+//              // e em seguida chamamos a o método addEventSource para montar o calendário na tela. 
+//               events.forEach(function(event) {
+//               var eventId = event.id;
+//               if (!uniqueEvents.has(eventId)) {
+//                 uniqueEvents.add(eventId);
+//                 calendar.addEventSource(event);
+//           }
+//           });
+//           }
+//         }
+//       ],
+//       eventLeave: function(info) {
+//         console.log('event left!', info.event);
+//       },
+//     });
 
-  var calendar1 = createCalendar(calendarEl1, 'Sala 1', FullCalendar);
-  var calendar2 = createCalendar(calendarEl2, 'Sala 2', FullCalendar);
-  var calendar3 = createCalendar(calendarEl3, 'Sala 3', FullCalendar);
+//     return calendar;
+//   }
+
+//   var calendar1 = createCalendar(calendarEl1, 'Sala 1', FullCalendar);
+//   var calendar2 = createCalendar(calendarEl2, 'Sala 2', FullCalendar);
+//   var calendar3 = createCalendar(calendarEl3, 'Sala 3', FullCalendar);
 
 
-  calendar1.render();
-  calendar2.render();
-  calendar3.render();
+//   calendar1.render();
+//   calendar2.render();
+//   calendar3.render();
+// });
+
+
+fetch('./teste.json')
+.then(response => response.json())
+.then(data => {
+  var events = data.filter(function(event) {
+    return event.place === place;
+  });
+
+  events.forEach(event => {
+    event.beginEvent = event.start;
+    event.endEvent = event.end;
+    delete event.start;
+    delete event.end;
+  });
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,dayGridDay,listMonth'
+    },
+    initialView: 'dayGridDay',
+    locale: 'pt-br',
+    initialDate: dataFormatada,
+    editable: false,
+    eventContent: getEventContent,
+    events: events,
+    eventLeave: function(info) {
+      console.log('event left!', info.event);
+    },
+  });
+
+  calendar.render();
+})
+.catch(error => {
+  console.error('Erro ao carregar o arquivo JSON:', error);
+});
+}
+
+var calendar1 = createCalendar(calendarEl1, 'Sala 1', FullCalendar);
+var calendar2 = createCalendar(calendarEl2, 'Sala 2', FullCalendar);
+var calendar3 = createCalendar(calendarEl3, 'Sala 3', FullCalendar);
 });
