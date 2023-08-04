243f059969ad3a02dabef4d549fc706fc1a83d15
davi <davijluna@gmail.com>
2023-08-04 16:52:16 -0300

    fix: descomentando class source-calendar e destination-calendar

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl1 = document.getElementById('source-calendar');
-  // var calendarEl2 = document.getElementById('destination-calendar');
-  // var calendarEl3 = document.getElementById('tree-calendar');
+  var calendarEl2 = document.getElementById('destination-calendar');
+  var calendarEl3 = document.getElementById('tree-calendar');
 
   // var calendarEl4 = document.getElementById('four-calendar');
   // var calendarEl5 = document.getElementById('five-calendar');
   // var calendarEl6 = document.getElementById('six-calendar');
-//  criamos uma variável com a função para setar.
+
+// variável com a função para setar.
   var uniqueEvents = new Set();
 
   const selectedDayElement = document.getElementById('selectedDay'); //////////
@@ -87,13 +88,13 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   var calendar1 = createCalendar(calendarEl1, 'Sala 1', FullCalendar);
-  // var calendar2 = createCalendar(calendarEl2, 'Sala 2', FullCalendar);
-  // var calendar3 = createCalendar(calendarEl3, 'Sala 3', FullCalendar);
+  var calendar2 = createCalendar(calendarEl2, 'Sala 2', FullCalendar);
+  var calendar3 = createCalendar(calendarEl3, 'Sala 3', FullCalendar);
 
 
   calendar1.render();
-  // calendar2.render();
-  // calendar3.render();
+  calendar2.render();
+  calendar3.render();
 
   function setSelectedDay(date) {
     const selectedDate = moment(date).format('YYYY-MM-DD');
