27e935b9f0656a3ba73d2a1de5fa37f5f7e5a3ff
davi <davijluna@gmail.com>
2023-09-14 08:11:36 -0300

    feat: criando arquivo para criar e reinderizar calendário

---
+++ createAndRenderCalendar.js
@@ -0,0 +1,9 @@
+// export function createAndRenderCalendars(calendarEls, salaNames, FullCalendar) {
+//   const calendars = [];
+  
+//   for (let i = 0; i < salaNames.length; i++) {
+//     calendars[i] = createCalendar(calendarEls[i], salaNames [i], FullCalendar);
+//     calendars[i].render();
+//   }
+//   return calendars;
+// }
